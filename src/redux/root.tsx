d8a5ca9a0eac7d34edaa8458c87a5163d0e001b4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:06:15 +0300

    message store add

--- src/redux/root.tsx
+++ src/redux/root.tsx
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
+import { MessageDataStore } from "./store/roomMessage";
 import { UserDataStore } from "./store/jwt";
 import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
     jwt: UserDataStore,
+    message: MessageDataStore
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
