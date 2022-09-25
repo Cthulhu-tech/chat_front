2884ddc9c002d1b54ee5197b1fde7206f965fcd3
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:06:55 +0300

    new page add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import { SocketConnection } from "./components/connection/connection"
+import { Registration } from "./page/auth/registration/registration"
 import { ProtectedRouter } from "./components/protected/protected"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Registration } from "./page/registration/registration"
-import { Login } from "./page/login/login"
+import { Layout } from "./components/layout/layout"
+import { Login } from "./page/auth/login/login"
+import { NotFound } from "./components/404/404"
 import { Home } from "./page/home/home"
 import { Room } from "./page/room/room"
 
 export const App = () => {
   return <BrowserRouter>
     <Routes>
-        <Route element={<ProtectedRouter/>}>
-          
-          <Route element={<SocketConnection/>}>
-
-              <Route path="/" element={<Home/>}>
-                <Route path="/room/:id" element={<Room />}/>
-              </Route>
-            
+        <Route element={<Layout/>}>
+          <Route element={<ProtectedRouter/>}>
+            <Route element={<SocketConnection/>}>
+                <Route path="/" element={<Home/>}>
+                  <Route path="/room/:id" element={<Room />}/>
+                </Route>
+            </Route>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/registration" element={<Registration />}/>
           </Route>
-
-          <Route path="/login" element={<Login />}/>
-          <Route path="/registration" element={<Registration />}/>
+          <Route path="*" element={<NotFound/>}/>
         </Route>
     </Routes>
   </BrowserRouter>
