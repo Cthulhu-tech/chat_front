5d2aa902d6269284bda8226d07d6c3c956d8f4fa
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:04:13 +0300

    room logic update

--- src/page/room/room.tsx
+++ src/page/room/room.tsx
@@ -1,17 +1,42 @@
-import { SocketContext } from "../../context/socket";
-import { useContext, useState } from "react"
+import { setMessageData, updateMessageData } from "../../redux/store/roomMessage"
+import { Message, ReduxStore, RoomMessage } from "../../interface/redux"
+import { SocketContext } from "../../context/socket"
+import { MessageInput } from './messageInput'
+import { useContext, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { MessageList } from "./messageList"
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 
 export const Room = () => {
 
-    const [msg, setMsg] = useState("")
-    const socket = useContext(SocketContext);
+    const {id} = useParams()
+    const dispatch = useDispatch()
+    const socket = useContext(SocketContext)
+    const user = useSelector((store:ReduxStore) => store.jwt)
+
+    useEffect(() => {
+        if(user.token && user.user){
+            socket.emit("getAllMessage", {id})
+            socket.emit("joinRoom", {username: user.user, id})
+            socket.on("join", (msg) => console.log(msg, ' join'))
+            socket.on("leave", (msg) => console.log(msg, ' leave'))
+            socket.on("messageRoomGet", (msg: Message) => dispatch(updateMessageData(msg)))
+            socket.on("setAllMessage", (data: RoomMessage) => dispatch(setMessageData(data)))
+        }
+        return () => {
+            if(user.token && user.user){
+                socket.emit("leaveRoom", {username: user.user, id})
+                socket.off("join")
+                socket.off("leave")
+                socket.off("messageRoomGet")
+                socket.off("setAllMessage")
+            }
+        }
+    },[socket, id])
     
-    const sendMessage = () => socket.emit('message', {msg})
-
-    const handleMessage = (event: React.ChangeEvent<HTMLInputElement>) => setMsg(event.target.value)
-
-    return <main>
-        <input type="text" placeholder="message" value={msg} onChange={handleMessage}/>
-        <input type="button" value="send" onClick={sendMessage}/>
-    </main>
+    return <section>
+        <MessageList/>
+        <MessageInput/>
+    </section>
 }
