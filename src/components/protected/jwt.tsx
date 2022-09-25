7b1212cdfd98f8f2e40f482cba00fbebc174cf6f
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:01:28 +0300

    jwt logic update

--- src/components/protected/jwt.tsx
+++ src/components/protected/jwt.tsx
@@ -1,4 +1,4 @@
-import { deleteUserData, updateUserData } from "../../redux/store/jwt"
+import { updateUserData } from "../../redux/store/jwt"
 import { UserPayload } from "../../interface/redux"
 import { useFetch } from "../../hook/useFetch"
 import { useDispatch } from "react-redux"
@@ -9,12 +9,11 @@ import { useEffect } from "react"
 const JWT = () => {
 
     const dispatch = useDispatch()
-    const {load, data, error, fetchData} = useFetch<UserPayload>("POST")
+    const [load, data, error, fetchData] = useFetch<UserPayload>("POST")
 
     useEffect(() => {fetchData("user/refresh")}, [])
     useEffect(() => {
         if(data) dispatch(updateUserData(data))
-        else dispatch(deleteUserData(null))
     },[data, load])
     
     if(error) return <Error {...{message: error.message}}/>
