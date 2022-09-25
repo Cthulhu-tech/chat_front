7d04f33a9e9ab085d9e9c3034768a86f2ef63310
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:04:56 +0300

    message list add

---
+++ src/page/room/messageList.tsx
@@ -0,0 +1,25 @@
+import { ReduxStore } from "../../interface/redux"
+import { useMemo, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+export const MessageList = () => {
+
+    const {id} = useParams()
+    const messageList = useSelector((store:ReduxStore) => store.message)
+
+    useEffect(() => {console.log(messageList)}, [messageList])
+
+    const reverseData = messageList.roomMessage.slice().reverse()
+    
+    const List = useMemo(() => reverseData, [messageList])
+
+    return <article>{
+        List.map((message) => {
+            return <div key={message.id}>
+                <p>{message.message}</p>
+                <p>{message.user}</p>
+            </div>
+        })    
+    }</article>
+}
