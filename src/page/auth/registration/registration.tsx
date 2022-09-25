188595d6eedc4c9ec8e4d3c37ac6217eee151990
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:03:50 +0300

    registration component add

---
+++ src/page/auth/registration/registration.tsx
@@ -0,0 +1,26 @@
+import { updateUserData } from "../../../redux/store/jwt"
+import { UserPayload } from "../../../interface/redux"
+import { IDataInput } from "../../../interface/fetch"
+import { useFetch } from "../../../hook/useFetch"
+import { useForm } from "../../../hook/useForm"
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
+
+export const Registration = () => {
+
+    const dispatch = useDispatch()
+    const [value, formHandler] = useForm<IDataInput>()
+    const [load, data, error, fetchData] = useFetch<UserPayload, IDataInput>("POST")
+
+    const LoginData = () => fetchData("user/regist", value)
+
+    useEffect(() => {
+        if(data) dispatch(updateUserData(data))
+    },[data, load])
+
+    return <>
+        <input type="text" placeholder="login" onChange={formHandler} name="login"/>
+        <input type="text" placeholder="password" onChange={formHandler} name="password"/>
+        <input type="button" value="send" onClick={LoginData} />
+    </>
+}
