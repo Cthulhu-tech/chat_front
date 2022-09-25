4a42891e8f35c9be9a1a6f164d98a4a9447e123b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 13:58:58 +0300

    logic header end

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,16 @@
+import { NotDefined } from "../userNotDefined/notDefined"
+import { ReduxStore } from "../../../interface/redux"
+import { Defined } from "../userDefined/defined"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
+
+export const Header = () => {
+    
+    const userData = useSelector((store:ReduxStore) => store.jwt)
+
+    useEffect(() => {}, [userData])
+
+    return <header>
+        {!userData.load && (userData.token ? <Defined/> : <NotDefined/>)}
+    </header>
+}
