e3645f1de3cd3e929f121faa03536b99c0aacb45
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 05:31:04 +0300

    protected add

---
+++ src/components/protected/jwt.tsx
@@ -0,0 +1,25 @@
+import { deleteUserData, updateUserData } from "../../redux/store/jwt"
+import { UserPayload } from "../../interface/redux"
+import { useFetch } from "../../hook/useFetch"
+import { useDispatch } from "react-redux"
+import { Outlet } from "react-router-dom"
+import { Error } from "../error/error"
+import { useEffect } from "react"
+
+const JWT = () => {
+
+    const dispatch = useDispatch()
+    const {load, data, error, fetchData} = useFetch<UserPayload>("POST")
+
+    useEffect(() => {fetchData("user/refresh")}, [])
+    useEffect(() => {
+        if(data) dispatch(updateUserData(data))
+        else dispatch(deleteUserData(null))
+    },[data, load])
+    
+    if(error) return <Error {...{message: error.message}}/>
+
+    return <Outlet/>
+}
+
+export default JWT
--- src/components/protected/protected.tsx
+++ src/components/protected/protected.tsx
@@ -1,7 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate, useLocation } from "react-router-dom"
+import { lazy, Suspense, useEffect, } from "react"
+import { ReduxStore } from "../../interface/redux"
+import { Loading } from "../loading/loading"
+import { useSelector } from "react-redux"
+
+const JWT = lazy(() => import('./jwt'))
 
 export const ProtectedRouter = () => {
-    return <>
-        <Outlet/>
-    </>
+
+    const location = useLocation()
+    const userData = useSelector((store:ReduxStore) => store.jwt)
+    
+    useEffect(() => {},[userData])
+
+    if(userData.token && !userData.load && (location.pathname === '/login' || location.pathname === '/registration')) return <Navigate to='/'/>
+    if(userData.token === null && !userData.load && (location.pathname === '/login' || location.pathname === '/registration')) return <Outlet/>
+    if(userData.token === null && !userData.load) return <Navigate to='/login'/>
+
+    return <Suspense fallback={<Loading/>}>
+        {userData.token && !userData.load ? <Outlet/> : <JWT/>}
+    </Suspense>
+
 }
