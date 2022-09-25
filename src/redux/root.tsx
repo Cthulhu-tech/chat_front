ac9cc407eb9a05513ab1c73b8c42f32bda940782
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 05:33:47 +0300

    root add

--- src/redux/root.tsx
+++ src/redux/root.tsx
@@ -1,11 +1,9 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { JWT } from "./store/jwt";
+import { UserDataStore } from "./store/jwt";
 import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
-    jwt: JWT,
+    jwt: UserDataStore,
 });
-
-export type AppDispatch = typeof store.dispatch
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
