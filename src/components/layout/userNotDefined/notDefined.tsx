a2239bf8030c901684a8a8babe16ae3352908b31
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:00:06 +0300

    user not defined header add

---
+++ src/components/layout/userNotDefined/notDefined.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom"
+
+export const NotDefined = () => {
+
+    return <div>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/registration">Registration</NavLink>
+    </div>
+
+}
