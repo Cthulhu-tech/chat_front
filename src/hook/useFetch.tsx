a8c7247afcde8d991960762a133db3a73f2e33b1
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 14:02:14 +0300

    useFetch type update

--- src/hook/useFetch.tsx
+++ src/hook/useFetch.tsx
@@ -1,41 +1,46 @@
-import { ErrorData, IUseFetch } from "../interface/redux"
-import { useEffect, useState } from "react"
+import { ErrorData, IUseFetch, ReduxStore } from "../interface/redux"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 
-export const useFetch = <T, U = any>(method: string = 'POST'): IUseFetch<T, U> => {
+export const useFetch = <T, U = any>(method: string = 'POST', Authorization: boolean = false): IUseFetch<T, U> => {
 
     const [load, setLoad] = useState(true)
     const [data, setData] = useState<T>()
     const [error, setError] = useState<ErrorData>()
 
-    const options: RequestInit = {
-        method,
-        mode: 'cors',
-        redirect: 'follow',
-        credentials: "include",
-        headers: {
-            'Content-type': 'application/json',
-            'Accept': 'application/json'
-        },
-    }
-
-    useEffect(() => {console.log(data)},[data, load, error])
+    const token = useSelector((store:ReduxStore) => store.jwt.token)
 
     const fetchData = (url: string, body?: U) => {
 
-        if(body)options.body = JSON.stringify(body);
+        const headers: HeadersInit = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+        }
 
+        if(Authorization) headers['Authorization'] = 'Bearer ' + token
+
+        const options: RequestInit = {
+            method,
+            mode: 'cors',
+            redirect: 'follow',
+            credentials: "include",
+            headers
+        }
+
+        if(body)options.body = JSON.stringify(body);
+        
         fetch(process.env.REACT_APP_Server_Response + url, options)
         .then(response => response.json())
         .then((json) => {
-            setData(json)
             setLoad(false)
+            setData(json)
         })
         .catch(err => {
-            setError(err.message)
             setLoad(false)
+            setError(err.message)
         })
 
     }
     
-    return {load, data, error, fetchData}
+    return [load, data, error, fetchData]
 }
