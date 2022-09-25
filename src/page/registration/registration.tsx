4651cf39c9ddb3cbed173988713e9bdf21d6c109
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-25 05:33:15 +0300

    registration add

--- src/page/registration/registration.tsx
+++ src/page/registration/registration.tsx
@@ -1,3 +1,23 @@
+import { UserPayload } from "../../interface/redux"
+import { IDataInput } from "../../interface/fetch"
+import { useFetch } from "../../hook/useFetch"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+
 export const Registration = () => {
-    return <>regist</>
+
+    const dispatch = useDispatch()
+    const [value, setValue] = useState<IDataInput>({login: "", password: ""})
+    const {load, data, error, fetchData} = useFetch<UserPayload, IDataInput>("POST")
+
+    const LoginData = () => fetchData("user/login") 
+
+    useEffect(() => {
+
+    },[data, load])
+
+    return <>
+        <input type="text" placeholder="login"/>
+        <input type="text" placeholder="password"/>
+    </>
 }
