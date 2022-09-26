547eca6b431f494ef468dac2eeeebd0548e98ea9
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-26 16:35:19 +0300

    conection logic update

--- src/components/connection/connection.tsx
+++ src/components/connection/connection.tsx
@@ -1,29 +1,33 @@
 import { socket, SocketContext } from "../../context/socket"
+import { Outlet, useLocation } from "react-router-dom"
 import { ReduxStore } from "../../interface/redux"
+import { useEffect, useState } from 'react'
 import { useSelector } from "react-redux"
-import { Outlet } from "react-router-dom"
-import { useEffect } from 'react'
+import { Error } from "../error/error"
 
 export const SocketConnection = () => {
 
+    const location = useLocation()
+    const [error, setError] = useState<{message: string}>();
     const jwt = useSelector((store:ReduxStore) => store.jwt.token)
 
     useEffect(() => {
         if(jwt){
             socket.auth = {jwt}
             socket.connect()
-            socket.on('error', (msg:string) => console.log(msg))
+            socket.on('error', (msg) => setError(msg))
             socket.on('create', (msg:string) => console.log(msg))
             socket.on('connection', (msg:string) => console.log(msg))
             socket.on('disconection', (msg:string) => console.log(msg))
         }
         return () => {
+            setError(undefined)
             jwt && socket.close()
         };
 
-    },[jwt])
+    },[jwt, location])
 
     return <SocketContext.Provider value={socket}>
-            <Outlet/>
+            {!error ? <Outlet/> : <Error {...error}/>}
     </SocketContext.Provider>
 }
