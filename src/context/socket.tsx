4edc5d3dbc6e1226da0509f7f9630f5b4f0f460f
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-27 05:09:18 +0300

    socket logic update

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 import { createContext } from "react";
 
-export const socket = io("http://localhost:3000",{transports: ['websocket'], upgrade: false});
+export const socket = io(`${process.env.REACT_APP_Server_Response}`,{transports: ['websocket'], upgrade: false});
 
 export const SocketContext = createContext<Socket>(socket);
